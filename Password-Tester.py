638d8895099147d0ed73334432f7c4c242e2deee
Ddesai013 <ddesai11@students.kennesaw.edu>
2021-12-07 23:23:10 -0500

    Finial update to code

--- Password-Tester.py
+++ Password-Tester.py
@@ -1,3 +1,5 @@
+import os
+
 
 
 from flask import Flask, render_template, request
@@ -15,34 +17,36 @@ def greet():
     for character in inputName:
         if character.isalpha():
             if character.isupper():
-                strength_score += 2
+                strength_score += 5
             else:   
-                strength_score += 1
+                strength_score += 2
         elif character.isdigit():
-            strength_score += 2
+            strength_score += 5
         else:
             strength_score += 6
 
     #gathers points on how long password is
     if len(inputName) < 5:
-        strength_score += 3
+        strength_score += 10
     elif len(inputName) >= 5 and len(inputName) < 8:
-        strength_score += 6
+        strength_score += 20
     elif len(inputName) >= 8 and len(inputName) < 11:
-        strength_score += 9
+        strength_score += 30
     else:
-        strength_score += 12
+        strength_score += 40
 
     #gathers score for password based on points collected
-    if strength_score < 15:
-        final_score = "Insufficent..."
-    elif strength_score >=15 and strength_score < 25:
-        final_score = "Sufficent."
-    else:
-        final_score = "Excellent!"
+    if strength_score < 25:
+        final_score = "Weak! Add more upper case characters, digits, or special characters!"
+    elif strength_score >=25 and strength_score < 50:
+        final_score = "Under Average! Keep adding more characters and digits!"
+    elif strength_score >=50 and strength_score < 75:
+        final_score = "Above Average! Password is strong but could be stronger with more characters/digits!"
+    else:    
+        final_score = "Excellent! Password is strong! Remember to copy and save your password in a secure location"
     
     #prints password given, points score, and final evaluation. Reminds user to save passcode in a secure location
-    inputName ="\'" + inputName + "\' is the password you inputted, and it has \na strength of " + str(strength_score) + "! " + final_score + "Remember to copy and save your password in a secure location"
+    inputName ="\n" + inputName + "\n is the password you inputed, and it has a strength of " + str(strength_score) + "! " + final_score
     return render_template("home.html",myName=inputName)
 
 @app.route('/') 
